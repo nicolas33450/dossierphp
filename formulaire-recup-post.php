5bb6444f25c492a6a99c400e1d6f6dbd3f4b07e8
nicolas33450 <40290258+nicolas33450@users.noreply.github.com>
2018-07-30 16:34:11 +0200

    g

--- formulaire-recup-post.php
+++ formulaire-recup-post.php
@@ -1,9 +1,64 @@
+<?php
+
+     function affiche_form($nom = '', $prenom = '', $ville = '', $verif = false){
+?>
+<form action="formulaire-recup-post.php" method="post">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-3 mt-5">
+                <label for="idn"> Nom : </label>
+            </div>
+            <div class="col-12 col-md-9 mt-5">
+                <input type="text" name="nom" id="idn" placeholder="Nom" value="<?= $nom ?>"><?=  etoile_rouge($nom, $verif) ?>
+            </div>
+
+            <div class="col-12 col-md-3">
+                <label for="idp"> Prenom : </label>
+            </div>
+
+            <div class="col-12 col-md-9">
+                <input type="text" name="prenom" id="idp" placeholder="Prenom" value="<?= $prenom ?>"><?=  etoile_rouge($prenom, $verif) ?>
+            </div>
+            <div class="col-12 col-md-3">
+                <label for="idv"> Ville : </label>
+            </div>
+            <div class="col-12 col-md-9">
+                <input type="text" name="ville" id="idv" placeholder="Ville" value="<?= $ville ?>"><?=  etoile_rouge($ville, $verif) ?>
+            </div>
+
+            <div class="col-12">
+                <button class="btn btn-secondary mt-3" type="submit" name="envoyer" value="Envoyer"> Envoyer </button>
+            </div>
+        </div>
+    </div>
+</form>
+<?php                                                                 
+}
+?>
+<?php
+    function etoile_rouge($champ, $verif = false)
+    { 
+        if(empty($champ) && ($verif /* == true */))
+        {
+            echo'<span class="rouge">*</span>';
+        }
+        else
+        {
+            echo '*';
+        }
+}
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="fr">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/formulaire-get.css">
         <title> Formulaire Get </title>
     </head>
 
@@ -13,47 +68,15 @@
         //on verifie que l'on a recu par la methode post une variable envoyer (bouton)
         if(isset($_POST['envoyer']))
         {
-            $n = $_POST['nom'];
-            $p = $_POST['prenom'];
-            $v = $_POST['ville']; 
+            $n = strtoupper(trim($_POST['nom']));// strtoupper : majuscule du nom
+            $p = ucfirst(trim($_POST['prenom']));// trim enleve les espaces avant et apres le nom// ucfirst: premire lettre en maj
+            $v = trim($_POST['ville']); 
 
-            if(($n == '') || ($p == '') || ($v == ''))
+            if(empty($n) || empty($p) || empty($v))//empty remplace le == ''
             {
-        ?>
-        <form action="formulaire-recup-post.php" method="post">
 
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 col-md-3 mt-5">
-                        <label for="idn"> Nom : </label>
-                    </div>
-                    <div class="col-12 col-md-9 mt-5">
-                        <input type="text" name="nom" id="idn" placeholder="Nom">
-                    </div>
-
-                    <div class="col-12 col-md-3">
-                        <label for="idp"> Prenom : </label>
-                    </div>
-
-                    <div class="col-12 col-md-9">
-                        <input type="text" name="prenom" id="idp" placeholder="Prenom">
-                    </div>
-                    <div class="col-12 col-md-3">
-                        <label for="idv"> Ville : </label>
-                    </div>
-                    <div class="col-12 col-md-9">
-                        <input type="text" name="ville" id="idv" placeholder="Ville">
-                    </div>
-
-                    <div class="col-12">
-                        <button class="btn btn-success mt-3" type="submit" name="envoyer" value="Envoyer"> Envoyer </button>
-                    </div>
-                </div>
-            </div>
-        </form>
-        
-        <?php
-
+                affiche_form($n, $p, $v, true);
+                
                 if($n == '')
                 {
                     echo 'veuillez saisir le nom <br/>';
@@ -89,7 +112,7 @@
                 <div class="col-12 col-md-3">
                     Ville : 
                 </div>
-                <div class="col-12 col-md-9 rouge">
+                <div class="col-12 col-md-9 ">
                     <?= $v; ?>
                 </div>
             </div>
@@ -100,48 +123,11 @@
         }
         else
         { 
-        ?>
 
-        <form action="formulaire-recup-post.php" method="post">
+            affiche_form();
 
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 col-md-3 mt-5">
-                        <label for="idn"> Nom : </label>
-                    </div>
-                    <div class="col-12 col-md-9 mt-5">
-                        <input type="text" name="nom" id="idn" placeholder="Nom">
-                    </div>
-
-                    <div class="col-12 col-md-3">
-                        <label for="idp"> Prenom : </label>
-                    </div>
-
-                    <div class="col-12 col-md-9">
-                        <input type="text" name="prenom" id="idp" placeholder="Prenom">
-                    </div>
-                    <div class="col-12 col-md-3">
-                        <label for="idv"> Ville : </label>
-                    </div>
-                    <div class="col-12 col-md-9">
-                        <input type="text" name="ville" id="idv" placeholder="Ville">
-                    </div>
-
-                    <div class="col-12">
-                        <button class="btn btn-success mt-3" type="submit" name="envoyer" value="Envoyer"> Envoyer </button>
-                    </div>
-                </div>
-            </div>
-        </form>
-
-        <?php
         }
         ?>    
-
-
-
-
-
 
         <!--<div class="col-12 col-md-3">
 Ville
