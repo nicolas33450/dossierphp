6a1523df68649b817f938a3dc7f31e2d50ebfb34
nicolas33450 <40290258+nicolas33450@users.noreply.github.com>
2018-07-30 11:30:54 +0200

    Initial commit

---
+++ premier_php.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> premier PHP </title>
+    </head>
+
+    <body>
+        du code HTML<br/>
+        <?php 
+        $abc = 12;
+        $def = 3.14;
+        $ghi = 'un simple texte';
+        
+        echo 'Du code PHP : ' . $ghi . '<br/>';
+        
+        for ($i = 1; $i <= 10; $i++)
+        {
+            echo 'i = ' . $i . '<br/>';
+            echo "i = $i <br/>";
+        }
+        
+        
+        ?>
+    </body>
+</html>
