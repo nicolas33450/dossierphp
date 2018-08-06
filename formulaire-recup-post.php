f79c6f1a29f365b877d71cd055e32235f0c1dbe7
nicolas33450 <40290258+nicolas33450@users.noreply.github.com>
2018-08-06 13:40:26 +0200

    2

--- formulaire-recup-post.php
+++ formulaire-recup-post.php
@@ -1,56 +1,89 @@
 <?php
 
-     function affiche_form($nom = '', $prenom = '', $ville = '', $verif = false){
+function affiche_form($nom = '', $prenom = '', $login = '',$mdp = '', $date_naissance = '',$tel = '', $email = '', $role = '' , $admin = '', $moderateur ='', $verif = false){
 ?>
 <form action="formulaire-recup-post.php" method="post">
 
     <div class="container">
         <div class="row">
+
+            <div class="col-12 text-center">
+                <h2> Création </h2>
+            </div>
             <div class="col-12 col-md-3 mt-5">
                 <label for="idn"> Nom : </label>
             </div>
             <div class="col-12 col-md-9 mt-5">
                 <input type="text" name="nom" id="idn" placeholder="Nom" value="<?= $nom ?>"><?=  etoile_rouge($nom, $verif) ?>
             </div>
-
             <div class="col-12 col-md-3">
                 <label for="idp"> Prenom : </label>
             </div>
-
             <div class="col-12 col-md-9">
                 <input type="text" name="prenom" id="idp" placeholder="Prenom" value="<?= $prenom ?>"><?=  etoile_rouge($prenom, $verif) ?>
             </div>
             <div class="col-12 col-md-3">
-                <label for="idv"> Ville : </label>
+                <label for="idl"> Login : </label>
             </div>
             <div class="col-12 col-md-9">
-                <input type="text" name="ville" id="idv" placeholder="Ville" value="<?= $ville ?>"><?=  etoile_rouge($ville, $verif) ?>
+                <input type="text" name="login" id="idl" placeholder="login" value="<?= $login ?>"><?=  etoile_rouge($login, $verif) ?>
+            </div>
+            <div class="col-12 col-md-3">
+                <label for="idm"> Mot de passe : </label>
+            </div>
+            <div class="col-12 col-md-9">
+                <input type="text" name="mdp" id="idmdp" placeholder="mdp" value="<?= $mdp ?>"><?=  etoile_rouge($mdp, $verif) ?>
+            </div>
+            <div class="col-12 col-md-3">
+                <label for="iddn"> Date naissance : </label>
+            </div>
+            <div class="col-12 col-md-9">
+                <input type="text" name="date_naissance" id="iddn" placeholder="date naissance" value="<?= $date_naissance ?>"><?=  etoile_rouge($date_naissance, $verif) ?>
+            </div>
+            <div class="col-12 col-md-3">
+                <label for="idt"> Tel : </label>
+            </div>
+            <div class="col-12 col-md-9">
+                <input type="text" name="tel" id="idt" placeholder="tel" value="<?= $tel ?>"><?=  etoile_rouge($tel, $verif) ?>
+            </div>
+            <div class="col-12 col-md-3">
+                <label for="ide"> Email : </label>
+            </div>
+            <div class="col-12 col-md-9">
+                <input type="text" name="email" id="ide" placeholder="email" value="<?= $email ?>"><?=  etoile_rouge($email, $verif) ?>
+            </div>
+            <div class="col-12 col-md-3">
+                <label for="idr"> Role : </label>
+            </div>
+            <div class="col-12 col-md-9">
+                <select name="role" id="idr">
+                    <option value="<? $admin ?>"> Admin </option>
+                    <option value="<? $moderateur ?>"> Moderateur </option>                   
+                </select><?=  etoile_rouge($email, $verif) ?>
             </div>
 
             <div class="col-12">
-                <button class="btn btn-secondary mt-3" type="submit" name="envoyer" value="Envoyer"> Envoyer </button>
+                <button class="btn btn-secondary mt-3" type="submit" name="envoyer" value="Envoyer"> Créer </button>
             </div>
         </div>
     </div>
 </form>
 <?php                                                                 
-}
-?>
+ }
+?>    
 <?php
-    function etoile_rouge($champ, $verif = false)
-    { 
-        if(empty($champ) && ($verif /* == true */))
-        {
-            echo'<span class="rouge">*</span>';
-        }
-        else
-        {
-            echo '*';
-        }
+function etoile_rouge($champ, $verif = false)
+{ 
+    if(empty($champ) && ($verif /* == true */))
+    {
+        echo'<span class="rouge">*</span>';
+    }
+    else
+    {
+        echo '*';
+    }
 }
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -70,24 +103,49 @@
         {
             $n = strtoupper(trim($_POST['nom']));// strtoupper : majuscule du nom
             $p = ucfirst(trim($_POST['prenom']));// trim enleve les espaces avant et apres le nom// ucfirst: premire lettre en maj
-            $v = trim($_POST['ville']); 
+            $l = trim($_POST['login']); 
+            $m = trim($_POST['mdp']); 
+            $dn = trim($_POST['date_naissance']); 
+            $t = trim($_POST['tel']); 
+            $e = trim($_POST['email']); 
+            $r = trim($_POST['role']); 
 
-            if(empty($n) || empty($p) || empty($v))//empty remplace le == ''
+            if(empty($n) || empty($p) || empty($l)|| empty($m) || empty($dn) || empty($t) || empty($e) || empty($admin) || empty($moderateur)) //empty remplace le == ''
             {
 
-                affiche_form($n, $p, $v, true);
-                
+                affiche_form($n, $p, $l, $m, $dn, $t, $e, $r, true);
+
                 if($n == '')
                 {
-                    echo 'veuillez saisir le nom <br/>';
+                    echo 'veuillez saisir votre nom <br/>';
                 }
                 if($p == '')
                 {
-                    echo 'veuillez saisir le prenom <br/>';
+                    echo 'veuillez saisir votre prenom <br/>';
                 }
-                if($v == '')
+                if($l == '')
                 {
-                    echo 'veuillez saisir la ville <br/>';
+                    echo 'veuillez saisir votre login <br/>';
+                }
+                if($m == '')
+                {
+                    echo 'veuillez saisir votre mot de passe <br/>';
+                }
+                if($dn == '')
+                {
+                    echo 'veuillez saisir votre date de naissance <br/>';
+                }
+                if($t == '')
+                {
+                    echo 'veuillez saisir votre telephone <br/>';
+                }
+                if($e == '')
+                {
+                    echo 'veuillez saisir votre email <br/>';
+                }
+                if($r == '')
+                {
+                    echo 'veuillez saisir votre role <br/>';
                 }
             }
             else
@@ -102,19 +160,50 @@
                 <div class="col-12 col-md-9">
                     <?= $n; ?>
                 </div>
-
                 <div class="col-12 col-md-3">
                     Prénom : 
                 </div>
                 <div class="col-12 col-md-9">
                     <?= $p; ?>
-                </div>
+                </div>               
                 <div class="col-12 col-md-3">
-                    Ville : 
+                    login : 
                 </div>
                 <div class="col-12 col-md-9 ">
-                    <?= $v; ?>
+                    <?= $l; ?>
                 </div>
+                <div class="col-12 col-md-3">
+                    mdp : 
+                </div>
+                <div class="col-12 col-md-9 ">
+                    <?= $m; ?>
+                </div>
+                <div class="col-12 col-md-3">
+                    date_naissance : 
+                </div>
+                <div class="col-12 col-md-9 ">
+                    <?= $dn; ?>
+                </div>
+                <div class="col-12 col-md-3">
+                    telephone : 
+                </div>
+                <div class="col-12 col-md-9 ">
+                    <?= $t; ?>
+                </div>
+                <div class="col-12 col-md-3">
+                    email : 
+                </div>
+                <div class="col-12 col-md-9 ">
+                    <?= $e; ?>
+                </div>
+                <div class="col-12 col-md-3">
+                    role : 
+                </div>
+                <div class="col-12 col-md-9 ">
+                    <?= $r; ?>
+                </div>
+
+
             </div>
         </div>
 
